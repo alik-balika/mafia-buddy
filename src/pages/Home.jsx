548c559915a7ab4205fe51777c7f578ee0808c36
alik-balika <balikaalik@gmail.com>
2025-04-29 21:26:34 -0700

    Make home buttons navigate to respective pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+
 import Button from "../components/Button";
+import { useNavigate } from "react-router";
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="bg-linear-to-b from-gray-900 to-gray-950 text-white h-screen p-8">
       <header className="text-center">
@@ -9,8 +13,8 @@ const Home = () => {
         <hr class="h-1 mx-auto my-4 border-0 dark:bg-gray-700"></hr>
       </header>
       <div className="flex flex-col gap-4">
-        <Button>Create a room</Button>
-        <Button>Join a room</Button>
+        <Button onClick={() => navigate("/create-room")}>Create a room</Button>
+        <Button onClick={() => navigate("/join-room")}>Join a room</Button>
       </div>
     </div>
   );
