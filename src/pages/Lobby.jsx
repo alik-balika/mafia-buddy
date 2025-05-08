32d3b2626e831b1ae59662479d8ae4a7d995a3f4
alik-balika <balikaalik@gmail.com>
2025-05-07 21:58:46 -0700

    Started working on lobby page

--- src/pages/Lobby.jsx
+++ src/pages/Lobby.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useParams } from "react-router";
+import Button from "../components/Button";
+
+import { Copy } from "lucide-react";
 
 const Lobby = () => {
-  return <div>Lobby</div>;
+  const { roomId } = useParams();
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center mb-2">{roomId}</h1>
+      <div className="flex justify-center gap-4">
+        <Button
+          bgColor="accent-gold"
+          className="text-black font-semibold flex gap-2"
+        >
+          <Copy /> Copy Invite Link
+        </Button>
+        <Button>Leave Room</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Lobby;
